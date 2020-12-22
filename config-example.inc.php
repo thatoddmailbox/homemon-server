03ef40087ddf4159d6d3e02b4c24044aeab6ffc2
Alex Studer <alex@studer.dev>
2020-12-22 17:52:00 -0500

    add sample config and database connection code

---
+++ config-example.inc.php
@@ -0,0 +1,7 @@
+<?php
+const DATABASE_HOST = "localhost";
+const DATABASE_USERNAME = "homemon-server";
+const DATABASE_PASSWORD = "password";
+const DATABASE_NAME = "homemon";
+
+const IP_FORWARDING_HEADER = "";
---
+++ db.inc.php
@@ -0,0 +1,4 @@
+<?php
+$db = new PDO("mysql:host=" . DATABASE_HOST . ";dbname=" . DATABASE_NAME . ";charset=utf8mb4", DATABASE_USERNAME, DATABASE_PASSWORD);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
