4c2991c5fccb534d3c014b9232fa90c886d1cf3d
Alex Studer <alex@studer.dev>
2020-12-22 21:59:29 -0500

    add battery level, battery voltage, and powered boolean to reports

--- report.php
+++ report.php
@@ -17,11 +17,41 @@ if ($providedToken == "" || !hash_equals(REPORT_TOKEN, $providedToken)) {
 	));
 }
 
+$poweredInput = $_GET["p"];
+if ($poweredInput != "0" && $poweredInput != "1") {
+	write_response(array(
+		"status" => "error",
+		"error" => "invalid_params"
+	));
+}
+$powered = ($poweredInput == "1");
+
+$batteryLevelInput = $_GET["b"];
+if (!is_numeric($batteryLevelInput)) {
+	write_response(array(
+		"status" => "error",
+		"error" => "invalid_params"
+	));
+}
+$batteryLevel = intval($batteryLevelInput);
+
+$batteryVoltageInput = $_GET["v"];
+if (!is_numeric($batteryVoltageInput)) {
+	write_response(array(
+		"status" => "error",
+		"error" => "invalid_params"
+	));
+}
+$batteryVoltage = intval($batteryVoltageInput);
+
 $ip = get_ip_address();
 $timestamp = time();
 
-$stmt = $db->prepare("INSERT INTO reports(ip, `timestamp`) VALUES(?, ?)");
+$stmt = $db->prepare("INSERT INTO reports(powered, batteryLevel, batteryVoltage, ip, `timestamp`) VALUES(?, ?, ?, ?, ?)");
 $stmt->execute(array(
+	$powered ? "1" : "0",
+	$batteryLevel,
+	$batteryVoltage,
 	$ip,
 	$timestamp
 ));
