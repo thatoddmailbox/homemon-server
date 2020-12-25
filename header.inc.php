f520b8800cc27da6c79f0466b3366ee5de4ff895
Alex Studer <alex@studer.dev>
2020-12-24 22:55:05 -0500

    basic idea for ui (but it's hardcoded to always say the same thing)

--- header.inc.php
+++ header.inc.php
@@ -8,5 +8,6 @@
 
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 		<link href="css/bootstrap-icons.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
 	</head>
 	<body>
--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php require_once("auth.inc.php"); ?>
 <?php require_once("header.inc.php"); ?>
 <div class="container">
-	<p>Nothing to see here.</p>
+	<div class="mainInfo">
+		<div class="mainInfoHeader display-6">
+			<i class="bi bi-lg bi-check2-circle"></i> Your home is <em>online</em>.
+		</div>
+		<div class="mainInfoSubheader lead">
+			Last checkin: three minutes ago
+		</div>
+	</div>
 </div>
 <?php require_once("footer.inc.php"); ?>
