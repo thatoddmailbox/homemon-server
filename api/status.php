6527a8563635b39caec1bfa19bd5bfd20621ce00
Alex Studer <alex@studer.dev>
2024-12-24 15:39:55 -0500

    move newer state object into a function instead of in api/status

--- api/status.php
+++ api/status.php
@@ -4,27 +4,4 @@ header("Content-Type: application/json");
 require_once("../data.inc.php");
 require_once("../status.inc.php");
 
-// TODO: probably should clean up get_home_state
-$state = get_home_state();
-$reports = get_recent_checkins();
-
-// Verdict possibilities:
-// * 0 = off: have a negative report from the past 20 mins
-// * 1 = possible outage: no communication in the past 20 mins
-// * 2 = on: have a positive report from the past 20 mins
-// TODO: this sucks
-$verdict = 0;
-if ($state["status"]["text"] == "not powered") {
-	$verdict = 1;
-} elseif ($state["status"]["text"] == "online") {
-	$verdict = 2;
-}
-
-$verdictTimestamp = $state["lastCheckin"]["timestamp"];
-
-echo json_encode(array(
-	"status" => "ok",
-	"verdict" => $verdict,
-	"verdictTimestamp" => $verdictTimestamp,
-	"reports" => $reports
-));
+echo json_encode(get_modern_home_state());
--- status.inc.php
+++ status.inc.php
@@ -4,6 +4,36 @@ require_once("util.inc.php");
 
 const TIMEOUT_INTERVAL_SECONDS = 20 * 60;
 
+const VERDICT_OFF = 0;
+const VERDICT_POSSIBLE_OUTAGE = 1;
+const VERDICT_ON = 2;
+
+function get_modern_home_state() {
+	// TODO: probably should clean up get_home_state
+	$state = get_home_state();
+	$reports = get_recent_checkins();
+
+	// Verdict possibilities:
+	// * 0 = off: have a negative report from the past 20 mins
+	// * 1 = possible outage: no communication in the past 20 mins
+	// * 2 = on: have a positive report from the past 20 mins
+	// TODO: this sucks
+	$verdict = VERDICT_OFF;
+	if ($state["status"]["text"] == "not powered") {
+		$verdict = VERDICT_POSSIBLE_OUTAGE;
+	} elseif ($state["status"]["text"] == "online") {
+		$verdict = VERDICT_ON;
+	}
+
+	$verdictTimestamp = $state["lastCheckin"]["timestamp"];
+
+	return array(
+		"verdict" => $verdict,
+		"verdictTimestamp" => $verdictTimestamp,
+		"reports" => $reports
+	);
+}
+
 function get_home_state() {
 	$lastCheckin = get_last_checkin();
 	$lastCheckinDisplay = "unknown";
